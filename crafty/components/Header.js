6e253adc4fe11cca119cc0e86238c8f55bff8648
selmi-ahlem <selmiahlem1999@gmail.com>
2023-09-28 11:53:31 +0200

    first

---
+++ crafty/components/Header.js
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <View>
+      <Text>Header</Text>
+    </View>
+  )
+}
+
+export default Header
+
