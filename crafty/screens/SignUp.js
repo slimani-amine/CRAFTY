dd2a5d4afaed2b6f2725de3a43676ffe89474e9b
selmi-ahlem <selmiahlem1999@gmail.com>
2023-09-28 14:12:15 +0200

    sign up screen front-side

--- crafty/screens/SignUp.js
+++ crafty/screens/SignUp.js
@@ -1,28 +1,32 @@
 import React from "react";
 import { Text, View, TextInput, TouchableOpacity } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import { AntDesign } from '@expo/vector-icons';
 
 export default function SignUp({ navigation }) {
   return (
-    <View className=" w-full h-full bg-white items-center justify-center ">
-        <Text>Sign Up</Text>
-        <TextInput
-      className="mb-4 w-44  border border-grey-500 rounded-md"
+    <View className=" w-full h-full bg-white pl-3 mt-6  justify-center items-start ">
+      <Text className="font-bold text-2xl mb-3">Sign Up</Text>
+      <TextInput
+        className="mb-4 w-96 h-16  border  border-slate-500 rounded-md"
         placeholder="Name"
       />
       <TextInput
-        className="mb-4 w-44  border border-grey-500 rounded-md"
+        className="mb-4 w-96 h-16  border  border-slate-500 rounded-md"
         placeholder="Email"
       />
       <TextInput
-        className="mb-4 w-44 border border-slate-500 rounded-md"
+        className="mb-4 w-96 h-16 border border-slate-500 rounded-md"
         placeholder="Password"
       />
-      <TouchableOpacity 
-      className="bg-[#BF9B7A] text-white p-2 rounded"
-      onPress={() => navigation.navigate('SignIn')}>
-        <Text className="text-center text-white">SignUp</Text>
+      <TouchableOpacity>
+      <Text className="ml-12"> Already have an account ? </Text>
+      <AntDesign title="hello"/>
+      </TouchableOpacity>
+      <TouchableOpacity
+        className="bg-[#BF9B7A] text-white w-96 h-12 p-2 mt-7 rounded-full items-center"
+        onPress={() => navigation.navigate("SignIn")}
+      >
+        <Text className="text-center text-white font-bold">SignUp</Text>
       </TouchableOpacity>
     </View>
   );
